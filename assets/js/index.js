6827812a94c450c708199054d7867b17edc3f682
Dannie Vinther <davi@kea.dk>
2020-04-07 14:14:45 +0200

    intial

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-// let vh = window.innerHeight * 0.01;
-// document.documentElement.style.setProperty("--vh", `${vh}px`);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 window.addEventListener("load", () => {
   const pWrap = document.querySelector(".progresswrapper");
